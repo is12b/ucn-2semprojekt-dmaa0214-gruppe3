044d3ad6cef0ea148612d6ca994731ce1c9b7c61
nonames.dk@gmail.com <nonames.dk@gmail.com@02d692b4-0017-d05b-3976-37ab1325d904>
2014-11-12 16:02:20 +0000

    Lavet pakker til de forskellige lags test

--- trunk/src/testLayer/dbLayer/TestDBCar.java
+++ trunk/src/testLayer/dbLayer/TestDBCar.java
@@ -1,11 +1,11 @@
-package testLayer;
+package testLayer.dbLayer;
 
 import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
 
-public class TestDBSale {
+public class TestDBCar {
 
 	@Before
 	public void setUp() throws Exception {
--- trunk/src/testLayer/dbLayer/TestDBSale.java
+++ trunk/src/testLayer/dbLayer/TestDBSale.java
@@ -1,11 +1,11 @@
-package testLayer;
+package testLayer.dbLayer;
 
 import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
 
-public class TestDBCar {
+public class TestDBSale {
 
 	@Before
 	public void setUp() throws Exception {
