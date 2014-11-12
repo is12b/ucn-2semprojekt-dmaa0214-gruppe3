2b236e6ea7bd397da51ae5deff123f15b375ab0d
nonames.dk@gmail.com <nonames.dk@gmail.com@02d692b4-0017-d05b-3976-37ab1325d904>
2014-11-12 10:01:00 +0000

    Library fix

--- trunk/src/dbLayer/IFDBCar.java
+++ trunk/src/dbLayer/IFDBCar.java
@@ -1,14 +1,25 @@
-/**
- * 
- */
 package dbLayer;
 
+import java.util.ArrayList;
+
+import modelLayer.Car;
+import modelLayer.Customer;
+
 /**
- * Class for IFDBCar
+ * Interface for DBCar
  * 
  * @author Group 3, dmaa0214, UCN
  *
  */
 public interface IFDBCar {
 
+	
+	public ArrayList<Car> getCars(Customer customer);
+	
+	public Car getCarByRegNr(String regNr);
+	
+	public Car getCarByVin(String vin);
+	
+	public Car getCar(int id);
+		
 }
